bdc2d4f779a260a597cd06f813d53ba1788ad0a4
Ajay Viswanathan <ajatix@users.noreply.github.com>
2026-07-12 04:37:39 +0700

    [chore] adds java library modules (#8)
    * [chore] adds java library modules

---
+++ cortex-core/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+  id("java-library")
+  id("com.diffplug.spotless")
+}
+
+tasks {
+  java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
+  spotless {
+    java { googleJavaFormat() }
+    kotlinGradle { ktfmt().googleStyle() }
+  }
+  test { useJUnitPlatform() }
+}
+
+dependencies {
+  api(project(":cortex-api"))
+
+  testImplementation(platform("org.junit:junit-bom:6.0.0"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
