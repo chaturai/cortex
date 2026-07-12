a68fe82daa8c94ba9367b0baa5273662eaf4a136
Ajay Viswanathan <ajatix@users.noreply.github.com>
2026-07-12 22:53:51 +0700

    [chore] adds maven publish

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,8 +4,10 @@ plugins {
 
 repositories {
     gradlePluginPortal()
+    mavenCentral()
 }
 
 dependencies {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:8.8.0")
+    implementation("com.vanniktech:gradle-maven-publish-plugin:0.33.0")
 }
